b40b9c1a7d69084bea99b36416a959ebf2de05cb
bepyan <rlantia97@naver.com>
2021-03-08 14:20:56 +0900

    init

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+interface Props {
+    
+}
+
+const Home = (props: Props) => {
+    return (
+        <View>
+            <Text> welcome to home </Text>
+        </View>
+    )
+}
+
+export default Home
