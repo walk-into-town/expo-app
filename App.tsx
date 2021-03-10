8e698cec62ede6d919a9444b1e0f47f682682d02
bepyan <bepyan@naver.com>
2021-03-10 16:45:12 +0900

    [add] styled-components

--- App.tsx
+++ App.tsx
@@ -1,23 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from '@components/home';
+import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
+import Home from './src/components/Home';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Home/>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`
+
+export default App;
--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       [
+        'babel-plugin-styled-components',
         'module-resolver',
         {
           root: ['./src'],
