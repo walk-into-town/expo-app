15b8fc387f35ca253785a3545fd79dfc7fb22173
bepyan <bepyan@naver.com>
2021-03-11 16:46:40 +0900

    [update] nav bottom

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -7,14 +7,30 @@ import Home from '../screens/Home';
 import Campaign from '../screens/Campaign';
 import GamePlay from '../screens/GamePlay';
 import Ranking from '../screens/Ranking';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 
 const HomeScreen = () => {
+
+    const iconName: { [key: string]: [string, string] } = {
+        Home: ["ios-home-outline", "ios-home"],
+        Campaign: ["ios-flashlight-outline", "ios-flashlight"],
+        GamePlay: ["ios-game-controller-outline", "ios-game-controller"],
+        Ranking: ["ios-trophy-outline", "ios-trophy"]
+    }
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route: { name } }) => ({
+                tabBarIcon: ({ focused, color, size }) =>
+                    <Ionicons name={iconName[name][focused ? 1 : 0]} size={size} color={color} />
+            })}
+            tabBarOptions={{
+                activeTintColor: "#517fa4",
+                inactiveTintColor: 'gray',
+            }}>
             <Tab.Screen name="Home" component={Home} />
             <Tab.Screen name="Campaign" component={Campaign} />
             <Tab.Screen name="GamePlay" component={GamePlay} options={{ tabBarBadge: 3 }} />
