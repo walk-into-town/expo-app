0adfc9cfd6aa1b44001932bb32eacb115c6dc9d9
bepyan <bepyan@naver.com>
2021-03-26 15:47:32 +0900

    styled theme provider

--- src/navigation/CampaginStack.tsx
+++ src/navigation/CampaginStack.tsx
@@ -6,8 +6,6 @@ import MakeCampagin from '../components/Campagin/MakeCampagin';
 import MakePinPoint from '../components/Campagin/MakePinPoint';
 import MakeCoupon from '../components/Campagin/MakeCoupon';
 import SearchCampagin from '../components/Campagin/SearchCampagin';
-import { NavigationProp, useNavigation } from '@react-navigation/core';
-import { CampaginStackParamList } from '@types';
 import theme from '../style/theme';
 import { ThemeProvider } from 'styled-components';
 
@@ -16,18 +14,17 @@ const Stack = createStackNavigator();
 const CampaginStack = () => {
 
     return (
-        <ThemeProvider theme={theme}>
-            <Stack.Navigator >
-                <Stack.Screen name="Campagin" component={Campagin} />
-                <Stack.Screen name="MyCampagin" component={MyCampagin} />
+        <Stack.Navigator >
+            <Stack.Screen name="Campagin" component={Campagin} />
+            <Stack.Screen name="MyCampagin" component={MyCampagin} />
 
-                <Stack.Screen name="MakeCampagin" component={MakeCampagin} />
-                <Stack.Screen name="MakePinPoint" component={MakePinPoint} />
-                <Stack.Screen name="MakeCoupon" component={MakeCoupon} />
 
-                <Stack.Screen name="SearchCampagin" component={SearchCampagin} />
-            </Stack.Navigator>
-        </ThemeProvider>
+            <Stack.Screen name="MakeCampagin" component={MakeCampagin} />
+            <Stack.Screen name="MakePinPoint" component={MakePinPoint} />
+            <Stack.Screen name="MakeCoupon" component={MakeCoupon} />
+
+            <Stack.Screen name="SearchCampagin" component={SearchCampagin} />
+        </Stack.Navigator>
     )
 }
 
