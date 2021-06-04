906ab1b8478a5999abf62799651d05c87bd594a6
bepyan <65283190+bepyan@users.noreply.github.com>
2021-06-04 17:02:37 +0900

    내 쿠폰 사용 👐🏻 / Fix 필터 버그

--- src/components/MyCouponStack/CouponList.tsx
+++ src/components/MyCouponStack/CouponList.tsx
@@ -1,12 +1,12 @@
-import { Coupon } from '@types'
+import { MyCoupon } from '@types'
 import React from 'react'
 import { ListItem } from 'react-native-elements'
 import { Row, SubTitle, Text3 } from '../../atoms'
 import { toCommonDate } from '../../util'
 
 interface Props {
-    couponList: Coupon[]
-    openModal: (coupon: Coupon) => void
+    couponList: MyCoupon[]
+    openModal: (coupon: MyCoupon) => void
 }
 
 const CouponList = (props: Props) => {
@@ -15,7 +15,7 @@ const CouponList = (props: Props) => {
             {
                 props.couponList.map((v, idx) => (
                     <ListItem
-                        disabled={false}
+                        disabled={v.used}
                         key={idx}
                         bottomDivider style={{ marginHorizontal: 30, marginVertical: 4, borderRadius: 10 }}
                         containerStyle={{ borderRadius: 10 }}
@@ -25,7 +25,7 @@ const CouponList = (props: Props) => {
                         <ListItem.Content>
                             <Row>
                                 <SubTitle>{v.name}</SubTitle>
-                                <Text3 style={{ marginLeft: 10 }}>~{toCommonDate(v.endDate)}</Text3>
+                                <Text3 style={{ marginLeft: 10 }}>~ {toCommonDate(v.endDate)}</Text3>
                             </Row>
                             <Text3>{v.goods}</Text3>
                         </ListItem.Content>
--- src/components/MyCouponStack/CouponModalCard.tsx
+++ src/components/MyCouponStack/CouponModalCard.tsx
@@ -1,14 +1,13 @@
-import { Coupon } from '@types'
+import { MyCoupon } from '@types'
 import React from 'react'
 import { Image, View } from 'react-native'
-import { Card } from 'react-native-elements'
 import { ScrollView } from 'react-native-gesture-handler'
 import { colorCode, ConfirmAlert, SubmitButton, SubTitle, Text1, Text3, Title, WhiteView } from '../../atoms'
 import { toCommonDate } from '../../util'
 
 interface Props {
-    coupon?: Coupon
-
+    coupon?: MyCoupon
+    useCoupon: (cid: string) => void
 }
 
 const CouponModalCard = (props: Props) => {
@@ -16,12 +15,13 @@ const CouponModalCard = (props: Props) => {
     if (!coupon)
         return <Title>error</Title>
 
-    const onUse = () => {
-        ConfirmAlert({
-            title: "쿠폰을 사용하시겠습니까?",
-            subTitle: "사용 후에는 쿠폰이 사용처리되어 더이상 쓸 수 없습니다.",
-            onConfirm: () => { }
-        })
+    const onUse = (cid?: string) => {
+        if (cid)
+            ConfirmAlert({
+                title: "쿠폰을 사용하시겠습니까?",
+                subTitle: "사용 후에는 쿠폰이 사용처리되어 더이상 쓸 수 없습니다.",
+                onConfirm: () => props.useCoupon(cid)
+            })
     }
 
 
@@ -39,7 +39,7 @@ const CouponModalCard = (props: Props) => {
                         사용만료일 {toCommonDate(coupon.endDate)}
                     </Text3>
                 </View>
-                <SubmitButton title="쿠폰사용" onPress={onUse} />
+                <SubmitButton title="쿠폰사용" onPress={() => onUse(coupon.id)} option={{ disabled: coupon.used }} />
 
                 <Text3 style={{ marginVertical: 4 }}>- {coupon.description}</Text3>
                 <Text1 style={{ marginVertical: 4 }}>- 1인 1회 쿠폰 사용 가능합니다.</Text1>
